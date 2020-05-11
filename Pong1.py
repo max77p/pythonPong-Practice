12826e36555200d297f840ede720d99b9653d9a6
max77p <shanindrakumar@gmail.com>
2020-05-10 20:26:17 -0400

    added ball movement

--- Pong1.py
+++ Pong1.py
@@ -3,64 +3,95 @@ import turtle
 wn = turtle.Screen()
 wn.title("Pong by Shan")
 wn.bgcolor("black")
-wn.setup(width=800,height=600)
+wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#PaddleA
-paddle_a=turtle.Turtle()
+# PaddleA
+paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(-350,0)
+paddle_a.goto(-350, 0)
 
-#PaddleB
-paddle_b=turtle.Turtle()
+# PaddleB
+paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
-paddle_b.goto(350,0)
+paddle_b.goto(350, 0)
 
-#Ball
-ball=turtle.Turtle()
+# Ball
+ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
-ball.goto(0,0)
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
 
-#Paddle A move function
+# Paddle A move function
+
+
 def paddle_a_up():
-    y=paddle_a.ycor()
-    y+=20
+    y = paddle_a.ycor()
+    y += 20
     paddle_a.sety(y)
+
 
 def paddle_a_down():
-    y=paddle_a.ycor()
-    y-=20
+    y = paddle_a.ycor()
+    y -= 20
     paddle_a.sety(y)
 
-#Paddle B move function
+# Paddle B move function
+
+
 def paddle_b_up():
-    y=paddle_b.ycor()
-    y+=20
+    y = paddle_b.ycor()
+    y += 20
     paddle_b.sety(y)
+
 
 def paddle_b_down():
-    y=paddle_b.ycor()
-    y-=20
+    y = paddle_b.ycor()
+    y -= 20
     paddle_b.sety(y)
 
-#keyboard binding
+
+# keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
-#Main game loop
+# Main game loop
 while True:
     wn.update()
+    # move the ball
+    ball.setx(ball.xcor()+ball.dx)
+    ball.sety(ball.ycor()+ball.dy)
+    # border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    # Paddle and ball collisions
+    
