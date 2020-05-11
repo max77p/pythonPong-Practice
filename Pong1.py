5eecaea86a7c250ab99d1665502e21d43b6de723
max77p <shanindrakumar@gmail.com>
2020-05-10 20:03:48 -0400

    first commit

---
+++ Pong1.py
@@ -0,0 +1,66 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Shan")
+wn.bgcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+#PaddleA
+paddle_a=turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+#PaddleB
+paddle_b=turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+#Ball
+ball=turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
+#Paddle A move function
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y=paddle_a.ycor()
+    y-=20
+    paddle_a.sety(y)
+
+#Paddle B move function
+def paddle_b_up():
+    y=paddle_b.ycor()
+    y+=20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y=paddle_b.ycor()
+    y-=20
+    paddle_b.sety(y)
+
+#keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+#Main game loop
+while True:
+    wn.update()
