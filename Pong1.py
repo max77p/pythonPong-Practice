ac56ad9c55e59348ac562c8a4f02da16a2389f10
max77p <shanindrakumar@gmail.com>
2020-05-20 17:53:40 -0400

    finished simple pong game

--- Pong1.py
+++ Pong1.py
@@ -6,6 +6,11 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
+
 # PaddleA
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -34,9 +39,17 @@ ball.goto(0, 0)
 ball.dx = 2
 ball.dy = 2
 
+# Pen 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("PlayerA:0  PlayerB:0", align="center",font=("Courier",25,"normal"))
+
+
 # Paddle A move function
-
-
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -88,16 +101,22 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("PlayerA:{}  PlayerB:{}".format(score_a, score_b), align="center",font=("Courier",25,"normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("PlayerA:{}  PlayerB:{}".format(score_a, score_b), align="center",font=("Courier",25,"normal"))
 
     # Paddle and ball collisions
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 400 and ball.ycor() > paddle_b.ycor() - 40):
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
 
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 400 and ball.ycor() > paddle_a.ycor() - 40):
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
